b7bf24034833ab705a4629c4081aa8f491fef649
Shiaupiau <stu43005@gmail.com>
2017-11-13 19:35:08 +0800

    Update start.sh

--- client/start.sh
+++ client/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ ! -f /home/hath/client/data/client_login ]; then
+if [ ! -f /hath/data/client_login ]; then
 	if [ $HatH_KEY ]; then
-		echo "${HatH_KEY}" >> /home/hath/client/data/client_login
+		echo "${HatH_KEY}" >> /hath/data/client_login
 	fi
 fi
 
-java -jar HentaiAtHome.jar --port $HatH_PORT --cache-dir "/home/hath/client/cache" --data-dir "/home/hath/client/data" --download-dir "/home/hath/client/download" --log-dir "/home/hath/client/log" --temp-dir "/home/hath/client/temp"
+java -jar HentaiAtHome.jar --port $HatH_PORT --cache-dir "/hath/cache" --data-dir "/hath/data" --download-dir "/hath/download" --log-dir "/hath/log" --temp-dir "/hath/temp"
